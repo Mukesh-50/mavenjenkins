40a36b4896b17466a0d7fe00f0d77fbea77c4e16
Mukesh Otwani <mukeshotwani.50@gmail.com>
2021-04-29 13:01:39 +0530

    pushing code to github

---
+++ src/test/java/mavenforjenkins/UITest.java
@@ -0,0 +1,25 @@
+package mavenforjenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UITest 
+{
+
+	@Test
+	public void startBrowser()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.quit();
+	}
+	
+	
+}
