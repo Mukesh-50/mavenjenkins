cc0378f256b6a420a4e6d549d4198d74f7c74386
SuriTechM <94347195+SuriTechM@users.noreply.github.com>
2021-11-16 07:27:06 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -12,23 +12,23 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class UITest 
 {
 
-	@Parameters("Browser")
+	//@Parameters("Browser")
 	@Test
-	public void startBrowser(String browserName)
+	public void startBrowser()
 	{
-		System.out.println("Parameter value is "+browserName);
-		WebDriver driver=null;
+		//System.out.println("Parameter value is "+browserName);
+		//WebDriver driver=null;
 		
-		if(browserName.contains("Chrome"))
-		{
+		//if(browserName.contains("Chrome"))
+		//{
 			WebDriverManager.chromedriver().setup();
-			 driver=new ChromeDriver();
-		}
-		else if(browserName.contains("Edge"))
-		{
-			WebDriverManager.edgedriver().setup();
-			 driver=new EdgeDriver();
-		}
+			WebDriver driver =new ChromeDriver();
+		//}
+//		else if(browserName.contains("Edge"))
+//		{
+//			WebDriverManager.edgedriver().setup();
+//			 driver=new EdgeDriver();
+//		}
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
