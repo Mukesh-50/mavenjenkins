1e78adb9d3b813b54c80ac8be5704b3dd68ee973
OTS <131981730+selenium3bymukesh@users.noreply.github.com>
2023-09-16 15:32:12 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest 
 {
